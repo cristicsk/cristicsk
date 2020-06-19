3910ae0f5d682f1fe71b96d766a3625df40cb9ba
cristicsk <cristi_csk@yahoo.com>
2020-06-19 11:20:14 +0300

    Updates

---
+++ precache-manifest.a842d1336a8e2f60b5dd03401b0db20d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f9680f10fb7ea8174b68b425383a60",
+    "url": "/cristicsk/index.html"
+  },
+  {
+    "revision": "39b1ef8ad6853d9448ac",
+    "url": "/cristicsk/static/css/main.60623317.chunk.css"
+  },
+  {
+    "revision": "835244e8aa75d5becb51",
+    "url": "/cristicsk/static/js/2.f0ff7ca5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cristicsk/static/js/2.f0ff7ca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39b1ef8ad6853d9448ac",
+    "url": "/cristicsk/static/js/main.822c1521.chunk.js"
+  },
+  {
+    "revision": "a9a9448e3e4234d4b331",
+    "url": "/cristicsk/static/js/runtime-main.6573dfe4.js"
+  },
+  {
+    "revision": "b05b544cdc744a0e2bf30776c8fa3a54",
+    "url": "/cristicsk/static/media/css.b05b544c.png"
+  },
+  {
+    "revision": "3c2a0c4d7b5bd353cb2c33187dfe1565",
+    "url": "/cristicsk/static/media/cv.3c2a0c4d.jpg"
+  },
+  {
+    "revision": "0ab109de6a9da2f9fc7a872b80217c02",
+    "url": "/cristicsk/static/media/html.0ab109de.png"
+  },
+  {
+    "revision": "d1b3eb8c5ee8784b361d9f12223c8084",
+    "url": "/cristicsk/static/media/illustrator.d1b3eb8c.png"
+  },
+  {
+    "revision": "a55e6039080caa7c91ac795b4dd76e28",
+    "url": "/cristicsk/static/media/javascript.a55e6039.png"
+  },
+  {
+    "revision": "86ac2f2d3562c54d99f16b035fc65a8b",
+    "url": "/cristicsk/static/media/logo.86ac2f2d.png"
+  },
+  {
+    "revision": "f0815bc6b07b7f55a34378c84769fea0",
+    "url": "/cristicsk/static/media/photoshop.f0815bc6.png"
+  },
+  {
+    "revision": "f365f5c5f86f1be61cb13175cd9c5176",
+    "url": "/cristicsk/static/media/php.f365f5c5.png"
+  },
+  {
+    "revision": "40720f18662afa99a864192180159c9c",
+    "url": "/cristicsk/static/media/python.40720f18.png"
+  },
+  {
+    "revision": "25633d7d5c2addfba43df43a041ffba4",
+    "url": "/cristicsk/static/media/reactjs.25633d7d.png"
+  },
+  {
+    "revision": "e2248121c8413b8a56a0500d747224d2",
+    "url": "/cristicsk/static/media/redux.e2248121.png"
+  },
+  {
+    "revision": "b0a244b86ff3dec5a5e167903d3ebe51",
+    "url": "/cristicsk/static/media/rotate.b0a244b8.jpg"
+  }
+]);
